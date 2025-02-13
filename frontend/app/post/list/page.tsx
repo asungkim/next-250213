b0cfc044dab398b36dd6c2dd92a24f27e51b80c7
asungkim <asungkim007@naver.com>
2025-02-13 12:40:55 +0900

    schema.d.ts type use

--- frontend/app/post/list/page.tsx
+++ frontend/app/post/list/page.tsx
@@ -1,23 +1,8 @@
+import { components } from "@/src/lib/backend/apiV1/schema";
 import axios from "axios";
 
-type PostDto = {
-  id: number;
-  createDate: string;
-  modifyDate: string;
-  authorId: number;
-  authorName: string;
-  title: string;
-  published: boolean;
-  listed: boolean;
-};
-
-type PostItemPageDto = {
-  currentPageNo: number;
-  pageSize: number;
-  totalPages: number;
-  totalItems: number;
-  items: PostDto[];
-};
+type PostDto = components["schemas"]["PostDto"];
+type PostItemPageDto = components["schemas"]["PageDto"];
 
 export default async function Page() {
   // api 호출
@@ -45,7 +30,7 @@ export default async function Page() {
       <hr />
 
       <ul>
-        {pageDto.items.map((item: PostDto) => {
+        {pageDto.items?.map((item: PostDto) => {
           return (
             <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
               <div>id : {item.id}</div>
