48262d791f13f1123545fa548a33df1de1a404cb
asungkim <asungkim007@naver.com>
2025-02-14 09:34:07 +0900

    paging 2

--- frontend/app/post/list/page.tsx
+++ frontend/app/post/list/page.tsx
@@ -64,10 +64,10 @@ export default async function Page({
           defaultValue={keyword}
         />
         <input type="submit" value="검색" />
-        <label className="ml-5" htmlFor="pageSize">
+        <label className="ml-5" htmlFor="">
           페이지 당 행 개수 :
         </label>
-        <select name="pageSize">
+        <select name="pageSize" defaultValue={pageSize}>
           <option value="10">10</option>
           <option value="30">30</option>
           <option value="50">50</option>
@@ -77,7 +77,13 @@ export default async function Page({
       <div className="flex gap-3">
         {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
           (page) => {
-            return <Link href={`/post/list/?page=${page}`}>{page}</Link>;
+            return (
+              <Link
+                href={`/post/list?keywordType=${keywordType}&keyword=${keyword}&pageSize=${pageSize}&page=${page}`}
+              >
+                {page}
+              </Link>
+            );
           }
         )}
       </div>
