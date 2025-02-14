d65f2663590135ecf7d4aa3174ad53ac1db2f103
asungkim <kimasung143@gmail.com>
2025-02-14 14:49:21 +0900

    my info server fetch - not credentials, use header cookie

--- frontend/app/member/me/page.tsx
+++ frontend/app/member/me/page.tsx
@@ -1,5 +1,20 @@
+import client from "@/src/lib/client";
 import ClientPage from "./ClientPage";
+import { cookies } from "next/headers";
 
 export default async function Page() {
-  return <ClientPage />;
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  if (response.error) {
+    return <div>{response.error.msg}</div>;
+  }
+
+  const rsData = response.data;
+  const memberDto = rsData.data;
+
+  return <ClientPage me={memberDto} />;
 }
