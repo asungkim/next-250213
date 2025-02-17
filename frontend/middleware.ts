5746b37fb6092f6d3d7a0f0d5c32bbddc5c4b1b8
asungkim <kimasung143@gmail.com>
2025-02-17 19:56:15 +0900

    middleware auth check

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,30 +3,59 @@ import client from "./src/lib/client";
 import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-  const reqToken = request.cookies.get("accessToken");
+  const myCookies = await cookies();
+  const accessToken = myCookies.get("accessToken");
 
-  console.log(request.nextUrl.toString());
-  if (request.nextUrl.pathname.startsWith("/post/edit/")) {
-    return new NextResponse("로그인이 필요합니다.", {
-      status: 401,
-      headers: {
-        "Content-Type": "text/html; charset=utf-8",
-      },
-    });
+  let isExpired = true;
+  let payload = null;
+
+  if (accessToken) {
+    try {
+      const tokenParts = accessToken.value.split(".");
+      payload = JSON.parse(Buffer.from(tokenParts[1], "base64").toString());
+      const expTimestamp = payload.exp * 1000;
+      isExpired = Date.now() > expTimestamp;
+    } catch (e) {
+      console.log("파싱 중 오류 발생 : ", e);
+    }
   }
 
-  const nextResponse = NextResponse.next();
+  let isLogin = payload != null;
 
-  const response = await client.GET("/api/v1/members/me", {
+  console.log("----------------------");
+  console.log(isLogin, isExpired);
+
+  if (isLogin && isExpired) {
+    const nextResponse = NextResponse.next();
+
+    const response = await client.GET("/api/v1/members/me", {
+      headers: {
+        cookie: (await cookies()).toString(),
+      },
+    });
+
+    const spirngCookie = response.response.headers.getSetCookie();
+    nextResponse.headers.set("set-cookie", String(spirngCookie));
+  }
+
+  if (!isLogin && isProtectedRoute(request.nextUrl.pathname)) {
+    return createUnauthorizedResponse();
+  }
+}
+
+function createUnauthorizedResponse(): NextResponse {
+  return new NextResponse("로그인이 필요합니다.", {
+    status: 401,
     headers: {
-      cookie: (await cookies()).toString(),
+      "Content-Type": "text/html; charset=utf-8",
     },
   });
+}
 
-  const spirngCookie = response.response.headers.getSetCookie();
-  nextResponse.headers.set("set-cookie", String(spirngCookie));
-
-  return nextResponse;
+function isProtectedRoute(pathname: string) {
+  return (
+    pathname.startsWith("/post/write") || pathname.startsWith("/post/edit")
+  );
 }
 
 export const config = {
