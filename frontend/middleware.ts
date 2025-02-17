c938af647580d84268799e391cfe7984b29b5446
asungkim <kimasung143@gmail.com>
2025-02-17 19:04:29 +0900

    custom response

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,16 +5,15 @@ import { cookies } from "next/headers";
 export async function middleware(request: NextRequest) {
   const reqToken = request.cookies.get("accessToken");
 
-  // console.log(request.nextUrl.toString());
-
-  // if (request.nextUrl.pathname.startsWith("/post/edit/")) {
-  //   return new NextResponse("로그인이 필요합니다.", {
-  //     status: 401,
-  //     headers: {
-  //       "Content-Type": "text/html; charset=utf-8",
-  //     },
-  //   });
-  // }
+  console.log(request.nextUrl.toString());
+  if (request.nextUrl.pathname.startsWith("/post/edit/")) {
+    return new NextResponse("로그인이 필요합니다.", {
+      status: 401,
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+      },
+    });
+  }
 
   const nextResponse = NextResponse.next();
 
